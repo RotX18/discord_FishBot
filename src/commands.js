f6f856ed15c384faa6916beebf798597696b8251
RotX18 <ryanohtzexuan@gmail.com>
2021-09-18 21:15:21 +0800

    Removed slash commands

--- src/commands.js
+++
@@ -1,7 +0,0 @@
-//JS FILE TO CREATE AND DEPLOY COMMANDS
-//https://discordjs.guide/creating-your-bot/creating-commands.html#registering-commands
-
-//import statements
-const {SlashCommandBuilder} = require("@discordjs/builders");
-const {REST} = require("@discordjs/rest");
-const {Routes} = require();
